967701934808732f7c1d58e4e43bae23da046ab0
Dipesh0302 <dipesh.thali@amexstech.com>
2019-07-28 13:54:01 +0530

    database connection

--- showData.php
+++ showData.php
@@ -1,6 +1,27 @@
 <?php
 
-echo "name =".$_GET['name']."<br/>";
-echo "email =".$_GET['email']."<br/>";
-echo "phoneNumbers =".$_GET['phoneNumbers']."<br/>";
-echo "batch =".$_GET['batch']."<br/>";
+require_once('connection.php');
+
+$database = new Connection();
+
+$id = (int)$_POST['id'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$college = $_POST['college'];
+$phoneNumber = (int)$_POST['phoneNumber'];
+$mark = (int)$_POST['marks'];
+
+$query = "insert into student values ($id,'$name','$college','$email',$phoneNumber,$mark)";
+
+echo "query made = $query";
+$database->executeQuery($query,'insert');
+
+
+$sqlOutput = $database->executeQuery('select * from student','select');
+var_dump($sqlOutput);
+
+
+// echo "name =".$_GET['name']."<br/>";
+// echo "email =".$_GET['email']."<br/>";
+// echo "phoneNumbers =".$_GET['phoneNumbers']."<br/>";
+// echo "batch =".$_GET['batch']."<br/>";
